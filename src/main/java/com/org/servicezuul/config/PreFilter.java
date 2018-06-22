b7ec751ac4f0bc0a6da50a8fd6bb037324c2196c
wangpeijian <519955464@qq.com>
2018-06-22 18:32:33 +0800

    初始化

--- src/main/java/com/org/servicezuul/config/PreFilter.java
+++ src/main/java/com/org/servicezuul/config/PreFilter.java
@@ -1,7 +1,43 @@
 package com.org.servicezuul.config;
 
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * Create by wangpeijian on 2018/6/22.
  */
-public class PreFilter {
+@Component
+public class PreFilter extends ZuulFilter {
+
+    private static Logger log = LoggerFactory.getLogger(PreFilter.class);
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        log.info(String.format("%s >>> %s", request.getMethod(), request.getRequestURL().toString()));
+        log.info("ok");
+        return null;
+    }
 }
