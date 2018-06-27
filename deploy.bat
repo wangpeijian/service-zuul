0be682ddba88a87fb037892a9b540099525c941a
wangpeijian <519955464@qq.com>
2018-06-27 14:59:53 +0800

    初始化

--- deploy.bat
+++ deploy.bat
@@ -5,4 +5,4 @@ docker build -t service-zuul --build-arg port=9991 ./
 docker stop zuul9991
 docker rm zuul9991
 
-docker run -i --net=container:eureka9901 --name="zuul9991" -p 9991:9991 service-zuul
+docker run -i --net=container:eureka9901 --name="zuul9991"  service-zuul
